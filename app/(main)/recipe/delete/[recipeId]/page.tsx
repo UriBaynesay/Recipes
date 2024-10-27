85e604387faef8297c7a3b7a11dbd4222b185a0c
Uri Baynesay <uri552@gmail.com>
2024-10-27 13:53:45 +0200

    fix build error

--- app/(main)/recipe/delete/[recipeId]/page.tsx
+++ app/(main)/recipe/delete/[recipeId]/page.tsx
@@ -5,27 +5,27 @@ import { getRecipeById } from "../../db"
 const DeleteRecipePage = async ({
   params,
 }: {
-  params: { recipeId: string }
+  params: Promise<{ recipeId: string }>
 }) => {
-  const {recipeId} = await params
+  const { recipeId } = await params
   const recipe = await getRecipeById(recipeId)
   if (!recipe) redirect("/")
-    return (
-      <main className="grow m-4 md:m-12 mt-8 md:flex md:justify-center">
-        <form
-          action={deleteRecipeAction.bind(null, recipe?.id as string)}
-          className="flex flex-col items-center"
-        >
-          <h1>
-            Are you sure you want to delete{" "}
-            <span className="font-bold">{recipe?.title}</span> recipe
-          </h1>
-          <button type="submit" className="text-red-400">
-            Delete
-          </button>
-        </form>
-      </main>
-    )
+  return (
+    <main className="grow m-4 md:m-12 mt-8 md:flex md:justify-center">
+      <form
+        action={deleteRecipeAction.bind(null, recipe?.id as string)}
+        className="flex flex-col items-center"
+      >
+        <h1>
+          Are you sure you want to delete{" "}
+          <span className="font-bold">{recipe?.title}</span> recipe
+        </h1>
+        <button type="submit" className="text-red-400">
+          Delete
+        </button>
+      </form>
+    </main>
+  )
 }
 
 export default DeleteRecipePage
